426f39d6a17db576cdb77e9da916830a303c92b3
Jimyancn <jimyancn@qq.com>
2025-10-28 14:19:24 +0800

    添加加法功能，创建 add.h 和 add.cpp 文件，更新 CMakeLists.txt 以支持新库，修改 main.cpp 以调用加法函数

--- src/libs/add.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_H
-#define ADD_H
-
-/// 返回两个数的和
-double add(double a, double b);
-
-#endif // ADD_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 //demo-123145
 #include <iostream>
 #include "prime.h"
+#include "add.h"
 /// @brief Main entry point of the program
 /// @return 0 on success, 1 on invalid input
 int main() {
@@ -16,6 +17,14 @@ int main() {
         std::cout << x << " YES" << std::endl;
     else
         std::cout << x << " NO" << std::endl;
+
+    std::cout << "Please input two numbers for addition:";
+    double a = 0, b = 0;
+    if (!(std::cin >> a >> b)) {
+        std::cerr << "Invalid input" << std::endl;
+        return 1;
+    }
+    std::cout << "The sum is: " << add(a, b) << std::endl;
 
     return 0;
 }
